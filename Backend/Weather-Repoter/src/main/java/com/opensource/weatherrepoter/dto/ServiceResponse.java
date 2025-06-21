1df749b689056af57fb0c1349d310d6ea49cc076
Sandun <sanduusanjaya@gmail.com>
2025-06-21 11:09:54 +0530

    Added a service response

---
+++ Backend/Weather-Repoter/src/main/java/com/opensource/weatherrepoter/dto/ServiceResponse.java
@@ -0,0 +1,16 @@
+package com.opensource.weatherrepoter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+}
